cad41f998c2ee154a3bdaf698c94847e715bc78e
Ian293382 <moshou5571228@gmail.com>
2022-04-03 21:50:52 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
   validates :title, presence: true
   has_many :posts
   belongs_to :user
+
+  #會有很多會員 會有很多關係
+ has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_many :groups
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+#串連關係 會有很多（使用者加入或未加入）群組的關係 ,透過 群組加入來判別
+    has_many :group_relationships
+    has_many :participated_groups, :through => :group_relationships, :source => :group
+
+
 end
