4fb07212da81406fc4092ef6118569788f32cee0
Ian293382 <moshou5571228@gmail.com>
2022-04-04 00:11:39 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,54 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
 
-
-
   def new
-     @group = Group.find(params[:group_id])
+    @group = Group.find(params[:group_id])
     @post = Post.new
   end
 
-
   def create
-      @group = Group.find(params[:group_id])
-      @post = Post.new(post_params)
-      @post.group = @group
-      @post.user = current_user
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
 
-      if @post.save
-        redirect_to group_path(@group)
-      else
-        render :new
-      end
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
     end
+  end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
 
-    private
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
 
-    def post_params
-      params.require(:post).permit(:content)
+    if @post.update(post_params)
+      redirect_to account_posts_path ,notice: "Update success!"
+    else
+      render :edit
     end
 
   end
+
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post delete"
+    redirect_to account_posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
